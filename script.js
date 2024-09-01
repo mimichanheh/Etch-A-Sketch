ac3ea17629d1f5e95270c191a220cb5e831f7fb3
mimichanheh <chihayabrainrot@gmail.com>
2024-09-01 16:27:48 +0100

    added extra effect when hovering over the squares

--- script.js
+++ script.js
@@ -1,15 +1,6 @@
 const container=document.querySelector(".container")
-for(let i=0;i<16;i++){
-    const lineSquares=document.createElement("div");
-    lineSquares.setAttribute("class",`container${i}`);
-    for(let j=0;j<16;j++){
-        const square=document.createElement("div");
-        square.setAttribute("class","square");
-        lineSquares.appendChild(square);
-    }
-    container.appendChild(lineSquares)
-}
-function replaceGrid(squaresNumber){
+let squaresNumber=16;
+function displayGrid(squaresNumber){
     while(container.firstChild){
         container.removeChild(container.firstChild)
     }
@@ -17,8 +8,15 @@ function replaceGrid(squaresNumber){
         const lineSquares=document.createElement("div");
         lineSquares.setAttribute("class",`container${i}`);
         for(let j=0;j<squaresNumber;j++){
+            let num1=Math.floor(Math.random()*255);
+            let num2=Math.floor(Math.random()*255);
+            let num3=Math.floor(Math.random()*255);
             const square=document.createElement("div");
             square.setAttribute("class","square");
+            let counter=0.1;
+            square.onmouseover=()=>{square.style.opacity=counter;
+                counter=counter+0.1;
+                square.style.backgroundColor=`rgb(${num1}, ${num2}, ${num3})`}
             let size=((960/squaresNumber)-8)/2;
             square.style.padding=`${size}px`;
             lineSquares.appendChild(square);
@@ -26,13 +24,13 @@ function replaceGrid(squaresNumber){
         container.appendChild(lineSquares)
     }
 }
+displayGrid(squaresNumber);
 const button=document.querySelector("button");
-let squaresNumber=0;
 button.addEventListener("click",()=>{squaresNumber=prompt("enter the number of squares per side for the new grid:");
 squaresNumber=parseInt(squaresNumber);
 console.log(squaresNumber)
 if(squaresNumber!==0 && squaresNumber<=100){
-replaceGrid(squaresNumber)}
+displayGrid(squaresNumber)}
 else{
     alert("enter a valid number (between 1 and 100)")
 }});
